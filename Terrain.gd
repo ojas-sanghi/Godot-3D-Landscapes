722afda31612cb36d9b227621698980e4a072fe3
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-12-30 13:34:22 -0700

    add landscape and files

---
+++ Terrain.gd
@@ -0,0 +1,22 @@
+extends Spatial
+
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	OS.window_maximized = true
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if event.is_action_pressed("toggle_mouse_captured"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		get_tree().set_input_as_handled()
+
+	if event.is_action_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = not OS.window_fullscreen
+		get_tree().set_input_as_handled()
